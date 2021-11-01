a78df136d3b32488ef42ca9d36a9a2e02647e6d7
SUMIT SINGH <singhsumit28lko@gmail.com>
2021-11-01 18:44:42 +0530

    Initial commit

---
+++ src/test/java/com/springFramework/author_book/AuthorBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springFramework.author_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
